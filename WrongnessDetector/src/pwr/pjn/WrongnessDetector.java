84d14e66917abcb58867c6bba517f6cdab9c4584
kcomr_000 <kcomr_000@192.168.1.6>
2014-12-30 15:45:22 +0100

    init

---
+++ WrongnessDetector/src/pwr/pjn/WrongnessDetector.java
@@ -0,0 +1,16 @@
+package pwr.pjn;
+
+/**
+ *
+ * @author KonradOliwer
+ */
+public class WrongnessDetector {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
